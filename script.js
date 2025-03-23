5b56984f4f3e6fe69ebe05681247f96e917cf9f3
popovskaas <sara.popovska2707@gmail.com>
2025-03-23 12:09:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("login-btn").addEventListener("click", function() {
+  document.getElementById("login-form").classList.toggle("hidden");
+});
+
+document.getElementById("book-btn").addEventListener("click", function() {
+  document.getElementById("booking-form").classList.toggle("hidden");
+});
 
